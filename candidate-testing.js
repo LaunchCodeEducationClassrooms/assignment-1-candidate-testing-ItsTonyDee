b008d8f429cb2e0b1625148b9c00c8e6efa2b03f
Anthony Porter <anthony.porter.it@outlook.com>
2021-02-20 18:10:27 +0000

    Trail space taken from line 18 answer.

--- candidate-testing.js
+++ candidate-testing.js
@@ -7,7 +7,7 @@ let candidateName = "";
 
 // TODO 1.2a: Define question, correctAnswer, and candidateAnswer //
 let question = 'Who was the first American woman in space? ';
-let correctAnswer = 'Sally Ride ';
+let correctAnswer = 'Sally Ride';
 let candidateAnswer = " ";
 let questions = [ '1: Who was the first American woman in space? ',
                  '2: True or false: 5000 meters == 5 kilometers? ',
