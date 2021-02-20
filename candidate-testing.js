4989073df87e06c835ff29433e7a3ecc3939be86
Anthony Porter <anthony.porter.it@outlook.com>
2021-02-20 05:59:11 +0000

    Fixed more auto grader issues.

--- candidate-testing.js
+++ candidate-testing.js
@@ -6,9 +6,9 @@ const input = require('readline-sync');
 let candidateName = "";
 
 // TODO 1.2a: Define question, correctAnswer, and candidateAnswer //
-//let question = ("Who was the first American woman in space? ");
-//let correctAnswer = ("Sally Ride ");
-//let candidateAnswer = "";
+let question = "";
+let correctAnswer = "";
+let candidateAnswer = "";
 let questions = ["1: Who was the first American woman in space? ",
                  "2: True or false: 5000 meters = 5 kilometers? ",
                  "3: (5+3)/2*10 = ? ",
